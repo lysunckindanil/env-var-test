b46c5af2e7a9a4c52b8ad990c8ad89cf776d9b37
Danila <danil.lysunckin@gmail.com>
2025-01-12 22:18:33 +0500

    initial init

---
+++ src/main/java/org/example/envvartest/EnvVarTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.envvartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvVarTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnvVarTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/envvartest/EnvVarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.envvartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvVarTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
